00eada6875e6b32bedf38ed342a2fd4f8b9be135
Eduardo-Bender <103793216+Eduardo-Bender@users.noreply.github.com>
2025-10-15 09:54:19 -0300

    Create flash.ino

---
+++ flash/flash.ino
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <Ultrasonic.h>
+
+#define analogTempPin A0
+#define somTriggerPin 21
+#define somEchoPin 22
+
+
+//////////////////////////////////////////////////////////////////////////////
+///////////////////////// Shameless global variables /////////////////////////
+int valTemp= 0;
+double voltsTemp = 0;
+Ultrasonic ultrasonic(somTriggerPin, somEchoPin, 40000UL);
+
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  //temperaturaLoop();
+  somLoop();
+  // displayLoop();
+  // botaoLoop();
+  // numpadLoop();
+
+  delay(300);
+}
+
+void somLoop(){
+  float cmMsec, inMsec;
+  cmMsec = ultrasonic.read(CM);
+  inMsec = ultrasonic.read(INC);
+
+  Serial.print("Distancia em cm: ");
+  Serial.print(cmMsec);
+  Serial.println();
+}
+
+void temperaturaLoop() {  
+  valTemp = analogRead(analogTempPin); 
+  voltsTemp = map(valTemp, 0, 1023, 0, 3300);
+  Serial.println((voltsTemp-500) * 0.1, 1);
+}
